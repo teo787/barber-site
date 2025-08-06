5cac603c26eb4de23beed8073fdad796492e6c49
Juan Jose <juanjoseca0128@gmail.com>
2025-08-05 19:49:13 -0500

    Cambio en el html

--- barberia/settings.py
+++ barberia/settings.py
@@ -98,7 +98,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'barberia', 'static', 'Barberia','css')]
+STATICFILES_DIRS = [ os.path.join (BASE_DIR / 'barberia/static')]  
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    
 
 
--- barberia/static/Barberia/js/Carrusel.js
+++ barberia/static/Barberia/js/Carrusel.js
@@ -2,7 +2,9 @@ let index = 0;
 
 function moveSlide(direction) {
   const track = document.querySelector('.carousel-track');
-  const slides = document.querySelectorAll('.carousel-track img');
+  // Esta línea ahora selecciona tanto las imágenes como los videos
+  const slides = document.querySelectorAll('.carousel-track img, .carousel-track video');
+  
   index = (index + direction + slides.length) % slides.length;
   track.style.transform = `translateX(-${index * 100}%)`;
 }
