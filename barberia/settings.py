e72785752d6c94a070c4d4f7b361c3a1bdde85a7
Juan Jose <juanjoseca0128@gmail.com>
2026-04-21 19:51:01 -0500

    Cambio en settings

--- barberia/settings.py
+++ barberia/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['explicit-barber.onrender.com','localhost', '127.0.0.1', '*']
 
 
 # Application definition
@@ -105,7 +105,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join (BASE_DIR / 'barberia/static')]  
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
